45c1e5c553457d6a37b2f7e4989eb69e5f273d08
Sachintha Karunaratne <sachintha84@gmail.com>
2018-08-06 23:34:24 +1000

    added future match simulation

--- src/bayesian_elo_parameter_estimation.R
+++ src/bayesian_elo_parameter_estimation.R
@@ -3,12 +3,14 @@ library(bayesplot)
 curr_season <- 2018
 
 results <- past_results[season == curr_season,]
-
+future_schedule <- schedules[season == curr_season][!results, on = c('round','team1','team2')]
 
 # generate input data to pass on to Stan ----------------------------------
 
 round_ids <- results[, as.numeric(gsub(pattern = "Round (.*)",replacement = '\\1',x = round))]
+futr_round_ids <- future_schedule[, as.numeric(gsub(pattern = "Round (.*)",replacement = '\\1',x = round))]
 n_rounds <- uniqueN(round_ids)
+futr_n_rounds <- uniqueN(futr_round_ids)
   
 team_list <- sort(unique(union(results$team1,results$team2)))
 n_teams <- length(team_list)
@@ -16,6 +18,9 @@ n_teams <- length(team_list)
 # get the ids of teams
 team1_ids <- sapply(results$team1, function(team){which(team == team_list)})
 team2_ids <- sapply(results$team2, function(team){which(team == team_list)})
+futr_team1_ids <- sapply(future_schedule$team1, function(team){which(team == team_list)})
+futr_team2_ids <- sapply(future_schedule$team2, function(team){which(team == team_list)})
+
 
 # get scores
 team1_score <- results$score_team1
@@ -25,12 +30,15 @@ team1_win_indictr <- as.numeric(team1_score >= team2_score)
 # assemble input data to a list to be passed onto stan
 input_list_stan <- list(round_ids = round_ids, n_rounds = n_rounds, n_matches = nrow(results),
                         n_teams = n_teams, team1_ids = as.numeric(team1_ids),
-                        team2_ids = as.numeric(team2_ids), team1_win_indictr = team1_win_indictr)
+                        team2_ids = as.numeric(team2_ids), team1_win_indictr = team1_win_indictr,
+                        futr_team1_ids = futr_team1_ids, futr_team2_ids = futr_team2_ids,
+                        futr_round_ids = futr_round_ids, first_futr_round = futr_round_ids[1],
+                        futr_n_rounds = futr_n_rounds, futr_n_matches = nrow(future_schedule))
 
 
 # fit the stan model
 fit <- stan(file = 'src/bayesian_elo_parameter_estimation.stan', data = input_list_stan, 
-            iter = 1000, chains = 4)
+            iter = 5000, chains = 4)
 
 # do some plotting
 posterior <- as.matrix(fit)
@@ -47,4 +55,16 @@ mcmc_areas(posterior, regex_pars = paste0("elo_score\\[[[:digit:]]+,",team_id,"\
 #  plot the ELO distributions for all teams as of last round
 rnd <- n_rounds
 mcmc_areas(posterior, regex_pars = paste0("elo_score\\[",n_rounds,",[[:digit:]]+\\]")) 
+
+
+
+# calculate the probabilities of the future matches
+n_sims <- nrow(posterior)
+future_schedule[, team1_win_prob := sapply(1:nrow(future_schedule), function(match){
+  col_name <- paste0('futr_match_outcome[',match,']')  
+  num_sim_wins <- sum(posterior[,col_name])
+  num_sim_wins/n_sims})]
+
+
+future_schedule[round == 'Round 21',]
 
--- src/bayesian_elo_parameter_estimation.stan
+++ src/bayesian_elo_parameter_estimation.stan
@@ -6,6 +6,15 @@ data{
   int team1_ids[n_matches];
   int team2_ids[n_matches];
   int team1_win_indictr[n_matches];
+  
+  /* futr_ variables are used to simulate the upcoming scheduled matches */
+  int futr_n_matches;
+  int futr_team1_ids[futr_n_matches];
+  int futr_team2_ids[futr_n_matches];
+  int futr_n_rounds;
+  int first_futr_round;
+  int futr_round_ids[futr_n_matches];
+  
 }
 
 parameters{
@@ -80,6 +89,9 @@ model{
 
 generated quantities {
   vector[n_teams] elo_score[n_rounds]; // each round is a vector of elo scores
+  vector[n_teams] last_elo_score; // used to store 
+  int futr_match_outcome[futr_n_matches]; // 1 if team 1 wins 0 otherwise
+  vector[n_teams] futr_elo_score[futr_n_rounds]; // to store upcoming simulated elo scores
   
   for (n in 1:n_rounds)
     elo_score[n] = elo_pre_season;
@@ -121,4 +133,52 @@ generated quantities {
       elo_score[rnd][team2_id] = prev_elo_team2 - elo_delta_team1;
     }
   }
+  
+  /* simulate the rest of the tournament */
+   last_elo_score = elo_score[n_rounds];
+   
+   for (n in 1:futr_n_rounds)
+    futr_elo_score[n] = last_elo_score;
+    
+   for (match in 1:futr_n_matches){
+    int team1_id = futr_team1_ids[match];
+    int team2_id = futr_team2_ids[match];
+    int rnd_id = futr_round_ids[match]-first_futr_round+1; // offseting the rounds that have happends so far
+    
+    // some variable declarations
+    real prev_elo_team1;
+    real prev_elo_team2;
+    real elo_diff;
+    real d;
+    real logit_inv_d;
+    real elo_delta_team1;
+    
+    
+    
+    // get the previous elo for each team
+    if (rnd_id == 1){
+       prev_elo_team1 = last_elo_score[team1_id];
+       prev_elo_team2 = last_elo_score[team2_id];
+    }else {
+       prev_elo_team1 = futr_elo_score[rnd_id-1][team1_id];
+       prev_elo_team2 = futr_elo_score[rnd_id-1][team2_id];
+    }
+    
+    elo_diff = prev_elo_team1-prev_elo_team2; 
+    d = elo_diff/xi;
+    logit_inv_d = 1/(1+ 10^(-d));
+    
+    // simulate a random match outcome
+    futr_match_outcome[match] = bernoulli_rng(logit_inv_d);
+    
+    /* modify the elo score for team 1 and team 2. Will assign the modified value for all 
+    the future rounds. This is to handle the situations where some teams may not play in 
+    the imediate next rounds(s) */
+    elo_delta_team1 = K*(futr_match_outcome[match]-logit_inv_d);
+    for (rnd in rnd_id:futr_n_rounds){
+      futr_elo_score[rnd][team1_id] = prev_elo_team1 + elo_delta_team1;
+      futr_elo_score[rnd][team2_id] = prev_elo_team2 - elo_delta_team1;
+    }
+   }
+   
 }
